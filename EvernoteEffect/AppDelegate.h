10ac4dbd39b21149b3ee4962cdf79ee0f96dd7e7
Sungwhee Kim <kimsungwhee@gmail.com>
2014-03-30 01:02:56 +0900

    Initial Commit

---
+++ EvernoteEffect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EvernoteEffect
+//
+//  Created by Sungwhee Kim on 14-3-30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EvernoteEffect/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EvernoteEffect
+//
+//  Created by Sungwhee Kim on 14-3-30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
